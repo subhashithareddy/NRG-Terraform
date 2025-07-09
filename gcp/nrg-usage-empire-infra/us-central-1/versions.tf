74e342ae217122b85fda71e65ded82c15c973f24
Subhash <subhash2017@gmail.com>
2025-07-09 16:19:19 +0530

    vesrions updated with random provider

--- gcp/nrg-usage-empire-infra/us-central-1/versions.tf
+++ gcp/nrg-usage-empire-infra/us-central-1/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source = "hashicorp/google-beta"
       version = "~>6.42.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "~>3.7.2"
+    }
   }
   required_version = "1.12.2"
 }
